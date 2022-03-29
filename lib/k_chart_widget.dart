3a546fc378c1ee0e6e5300691387732d77a98bc6
Lucas Bianco <lucas.biancogs@hotmail.com>
2022-03-29 11:11:00 -0300

    Removes required from nowPrice

--- lib/k_chart_widget.dart
+++ lib/k_chart_widget.dart
@@ -29,7 +29,7 @@ class TimeFormat {
 
 class KChartWidget extends StatefulWidget {
   final List<KLineEntity>? datas;
-  final double nowPrice;
+  final double? nowPrice;
   final MainState mainState;
   final bool volHidden;
   final SecondaryState secondaryState;
@@ -60,7 +60,7 @@ class KChartWidget extends StatefulWidget {
     this.datas,
     this.chartStyle,
     this.chartColors, {
-    required this.nowPrice,
+    this.nowPrice,
     this.mainState = MainState.MA,
     this.secondaryState = SecondaryState.MACD,
     this.onSecondaryTap,
